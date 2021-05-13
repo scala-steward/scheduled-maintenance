577d39bace2e630f8c1695de1d4d080136310013
Brian Holt <bholt@dwolla.com>
2021-05-13 12:02:02 -0500

    use prettier HTML for human-friendly responses

--- core/src/main/scala/com/dwolla/scheduledmaintenance/Main.scala
+++ core/src/main/scala/com/dwolla/scheduledmaintenance/Main.scala
@@ -28,16 +28,113 @@ object Main {
 
     if(accept.exists(_.contains("text/html")))
       buildResponse("text/html",
-        """<html>
-          |  <head><title>Dwolla :: Scheduled Maintenance</title></head>
-          |  <body>
-          |    <h1>Dwolla</h1>
-          |    <p>
-          |      Services are temporarily unavailable while we perform scheduled
-          |      maintenance. See <a href="https://status.dwolla.com">status.dwolla.com</a>
-          |      for more information.
-          |    </p>
-          |  </body>
+        """<!doctype html>
+          |<html lang="en-US">
+          |
+          |<head>
+          |  <meta charset="utf-8">
+          |  <meta http-equiv="x-ua-compatible" content="ie=edge">
+          |  <title>Scheduled Maintenance Underway | Dwolla</title>
+          |  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+          |
+          |  <link href="https://fonts.googleapis.com/css?family=Poppins:300,500|Roboto:300" rel="stylesheet">
+          |
+          |  <style type="text/css">
+          |    h1, h2 {
+          |      font-family: Poppins, sans-serif;
+          |    }
+          |
+          |    h1 {
+          |      font-weight: 500;
+          |      line-height: 120px;
+          |      font-size: 110px;
+          |      margin: 0 0 30px;
+          |      color: #2a2d38;
+          |    }
+          |
+          |    h2 {
+          |      font-weight: 300;
+          |      line-height: 54px;
+          |      font-size: 40px;
+          |      margin: 0 0 35px;
+          |      color: #2b334e;
+          |    }
+          |
+          |    p {
+          |      margin: 0;
+          |    }
+          |
+          |    p a {
+          |      color: #ee874b;
+          |      text-decoration: none;
+          |    }
+          |
+          |    .container {
+          |      text-align: center;
+          |    }
+          |
+          |    .logo {
+          |      width: 169px;
+          |      margin: 35px 0 100px;
+          |    }
+          |
+          |    .short-desc {
+          |      max-width: 600px;
+          |      margin: 0 auto;
+          |      font-weight: 300;
+          |      line-height: 35px;
+          |      font-size: 23px;
+          |      font-family: Roboto, sans-serif;
+          |      color: #6a7282;
+          |    }
+          |
+          |  @media only screen and (max-width: 768px) {
+          |    h1 {
+          |      line-height: 70px;
+          |      font-size: 50px;
+          |      margin-bottom: 20px;
+          |    }
+          |
+          |    h2 {
+          |      margin-bottom: 20px;
+          |      line-height: 28px;
+          |      font-size: 18px;
+          |    }
+          |
+          |    .container{
+          |      margin: 0 30px;
+          |    }
+          |
+          |    .logo {
+          |      margin-bottom: 80px;
+          |    }
+          |
+          |    .short-desc {
+          |      max-width: 430px;
+          |      line-height: 30px;
+          |      font-size: 16px;
+          |    }
+          |  }
+          |  </style>
+          |</head>
+          |
+          |<body>
+          |  <section class="container">
+          |    <a href="https://status.dwolla.com">
+          |      <img src="https://cdn.dwolla.com/com/dist/images/global/dwolla-logo-full-color_8fa10429.svg" class="logo" alt="Dwolla">
+          |    </a>
+          |    <h1>503</h1>
+          |    <h2>Service Unavailable: Scheduled Maintenance</h2>
+          |    <div class="short-desc">
+          |      <p>
+          |        Services are temporarily unavailable while we perform scheduled
+          |        maintenance. See <a href="https://status.dwolla.com">status.dwolla.com</a>
+          |        for more information.
+          |      </p>
+          |    </div>
+          |  </section>
+          |</body>
+          |
           |</html>
           |""".stripMargin)
     else
